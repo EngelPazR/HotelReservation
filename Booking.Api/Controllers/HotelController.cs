df446b145221229501e521245e55dddfd8ac3e60
Engel Josué Paz Rodríguez <engelpazrodriguez@gmail.com>
2026-08-21 16:52:30 -0600

    Se sube el avance del proycto de Asp.Net que se lleva hasta el momento

---
+++ Booking.Api/Controllers/HotelController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using Booking.Domain.Models;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Booking.Api.Controllers
+{
+    [ApiController]
+    [Route ("api/[controller]")]
+    public class HotelController : Controller
+    {
+
+        public HotelController()
+        {
+
+        }
+        [HttpGet]
+        public IActionResult GetRooms()
+        {
+            return Ok("Hello from hotel controller");
+        }
+
+        private List<Hotel> GetHotels()
+        {
+            return new List<Hotel>
+            {
+                             
+                new Hotel {
+                Id = 1,
+                Name = "Holiday Inn",
+                Stars = 5,
+                Country = "Nicaragua",
+                City = "Fajardo"
+                }
+
+            };
+        }
+    }
+}
---
+++ Booking.Domain/Models/Hotel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Booking.Domain.Models
+{
+    public class Hotel
+    {
+        public Hotel()
+        {
+
+        }
+        public Hotel(string idname, int stars, string address)
+        {
+
+        }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Stars { get; set; }
+        public string Address { get; set; }
+
+        public string City {  get; set; }
+
+        public string Country { get; set; } = "Unknown";
+
+    }
+}
